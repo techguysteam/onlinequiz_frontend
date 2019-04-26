a76be39f1f5f970317ccb7a8ba903a87a459ad51
phudinhnguyen <phudinhnguyen@kms-technology.com>
2019-04-26 14:39:55 +0700

    Fluent validation

---
+++ ThiHuong.Logic/Validations/FluentValidation/AccountValidation.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThiHuong.Framework.Models;
+
+namespace ThiHuong.Logic.Validations.FluentValidation
+{
+    public class AccountValidation : AbstractValidator<Account>
+    {
+        private UnitOfWork unitOfWork;
+        public AccountValidation(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+
+            
+
+        }
+
+
+
+    }
+}
